dcd9d35c0777111bd4f151f9e036d8e99029a932
Spencer Young <spencer.young@sparcusa.com>
2021-05-29 07:10:33 -0400

    added a registration form on login page

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 # users/urls.py
 
 from django.conf.urls import url, include
-from users.views import dashboard, homepage
+from users.views import dashboard, homepage, register
 
 urlpatterns = [
     url(r"^accounts/", include("django.contrib.auth.urls")),
     url(r"^dashboard/", dashboard, name="dashboard"),
+    url(r"^register/", register, name="register"),
     url(r"", homepage, name="homepage")
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from users.forms import CustomUserCreationForm
 
 # Create your views here.
 def dashboard(request):
@@ -6,3 +9,16 @@ def dashboard(request):
 
 def homepage(request):
 	return render(request, "home.html")
+
+def register(request):
+    if request.method == "GET":
+        return render(
+            request, "users/register.html",
+            {"form": CustomUserCreationForm}
+        )
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
