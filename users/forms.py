5fbceed94eb21721515760e1b0f61ac1d338c0d4
Spencer Young <spencer.young@sparcusa.com>
2021-05-29 10:26:38 -0400

    Made profile model to extend user and allow for saving for information.

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,15 @@
 # users/forms.py
 
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from users.models import Profile
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ("email",)
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('discordName',)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from datetime import date
 
-# Create your models here.
+#obj.date_modified = datetime.now()
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	discordName = models.TextField(max_length=80, blank=True)
+	creation_date = models.DateField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+		instance.profile.creation_date = date.today()
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
