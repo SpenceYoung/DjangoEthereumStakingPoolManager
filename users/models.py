e77fef461a99fc7c7de8c04e5a410478767dd6f0
Spencer Young <spencer.young@sparcusa.com>
2021-06-03 09:59:13 -0400

    pool statistics page, imageField for profile

--- users/models.py
+++ users/models.py
@@ -1,20 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.core.files.storage import FileSystemStorage
 from django.dispatch import receiver
 from datetime import date
 
-#obj.date_modified = datetime.now()
+fs = FileSystemStorage(location='/media/photos')
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	discordName = models.TextField(max_length=80, blank=True)
 	creation_date = models.DateField(null=True, blank=True)
-	contributions = models.FloatField(null=True, blank=True)
-	rewards = models.FloatField(null=True, blank=True)
+	contribImage = models.ImageField(null=True, blank=True,storage=fs)
+	contributions = models.FloatField(null=True, blank=True, default = 0)
+	rewards = models.FloatField(null=True, blank=True, default = 0)
 
 class PoolManager(models.Model):
-	total = models.FloatField(null=True, blank=True)
+	cryptoName = models.TextField(max_length=80, blank=True)
+	total = models.FloatField(null=True, blank=True, default = 0)
+
+class Contribution(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	pool = models.OneToOneField(PoolManager, on_delete=models.CASCADE)
+	amount = models.FloatField(null=True, blank=True, default = 0)
+
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
