f0c4ed8af965369cce302ec4639862c82ca72193
Spencer Young <spencer.young@sparcusa.com>
2021-06-03 20:57:42 -0400

    added a quick script to create a pool

---
+++ createPool.py
@@ -0,0 +1,12 @@
+#Use the below command after you input your desired pool name for the "poolName" string
+#python manage.py shell < createPool.py
+from users.models import PoolManager
+
+def createNewPool():
+	poolName = "algo"#str(input("Enter the name (crypto ticker) of your new pool"))
+	poolName = poolName.upper()
+	pool = PoolManager(cryptoName = poolName, total= 0.0)
+	pool.save()
+
+createNewPool()
+
